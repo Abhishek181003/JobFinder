64c3678121e4f9cb29160e491c235a3ddaf90e68
Abhishek Ranjan <142088316+Abhishek181003@users.noreply.github.com>
2025-06-26 17:10:05 +0530

    Update AddJobPage.jsx

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,6 +1,6 @@
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
-import AddJob from '../components/Addjob';
+import AddJob from '../components/AddJob';
 
 function AddJobPage(){
     return (
